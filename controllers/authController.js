e3d9b7aef9a8a9953f326ae69e0a8945fd584e5e
Your Name <urielparavi75@gmail.com>
2022-10-26 23:17:31 +0300

    51-basic-register-controller-section-12

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,5 +1,9 @@
+import User from "../models/User.js";
+import { StatusCodes } from "http-status-codes";
+
 const register = async (req, res, next) => {
-  res.send("register user");
+  const user = await User.create(req.body);
+  res.status(StatusCodes.OK).json({ user });
 };
 
 const login = async (req, res, next) => {
