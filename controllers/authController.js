f5c582efbb342a97d25fa1125edc821529de3ea2
Your Name <urielparavi75@gmail.com>
2022-10-29 15:21:39 +0300

    play-55-intro-section-13

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,8 +1,21 @@
 import User from "../models/User.js";
 import { StatusCodes } from "http-status-codes";
+import { BadRequestError } from "../errors/index.js";
 
 const register = async (req, res, next) => {
-  const user = await User.create(req.body);
+  const { name, email, password } = req.body;
+
+  if (!name || !email || !password) {
+    //Since we using the express async errors package,
+    // Instead of going with next(error), we need to throw Error("the message")
+    // CustomAPIError => We still send the message, but also the status code
+    throw new BadRequestError("Please provide all values.");
+  }
+  const userAlreadyExists = await User.findOne({ email });
+  if (userAlreadyExists) {
+    throw new BadRequestError("Email already in use.");
+  }
+  const user = await User.create({ name, email, password });
   res.status(StatusCodes.OK).json({ user });
 };
 
