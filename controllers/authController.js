11115643c80a5545ba6be2c52a580590ceba90e8
Your Name <urielparavi75@gmail.com>
2022-10-30 11:17:12 +0200

    70.Concurrently

--- controllers/authController.js
+++ controllers/authController.js
@@ -16,7 +16,18 @@ const register = async (req, res, next) => {
     throw new BadRequestError("Email already in use.");
   }
   const user = await User.create({ name, email, password });
-  res.status(StatusCodes.OK).json({ user });
+  // We invoke it before we return the user
+  const token = user.createJWT();
+  res.status(StatusCodes.OK).json({
+    user: {
+      email: user.email,
+      lastName: user.lastName,
+      location: user.location,
+      name: user.name,
+    },
+    token,
+    location: user.location,
+  });
 };
 
 const login = async (req, res, next) => {
--- models/User.js
+++ models/User.js
@@ -1,6 +1,7 @@
 import mongoose from "mongoose";
 import validator from "validator";
 import bcrypt from "bcryptjs";
+import jwt from "jsonwebtoken";
 
 const UserSchema = new mongoose.Schema({
   name: {
@@ -23,6 +24,7 @@ const UserSchema = new mongoose.Schema({
     type: String,
     required: [true, "Please provide password."],
     minlength: 6,
+    select: false,
   },
   lastName: {
     type: String,
@@ -42,5 +44,12 @@ UserSchema.pre("save", async function () {
   const salt = await bcrypt.genSalt(10);
   this.password = await bcrypt.hash(this.password, salt);
 });
+// createJWT => it could be every name we want, and with this we get access our document
+UserSchema.methods.createJWT = function () {
+  // console.log(this); => our document
+  return jwt.sign({ userId: this._id }, process.env.JWT_SECRET, {
+    expiresIn: process.env.JWT_LIFETIME,
+  });
+};
 
 export default mongoose.model("User", UserSchema);
