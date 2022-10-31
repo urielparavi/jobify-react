2e6fc7da0b1771f7936568a1cf97f745f5b6bbe2
Your Name <urielparavi75@gmail.com>
2022-10-31 13:17:34 +0200

    71-fetch-error

--- server.js
+++ server.js
@@ -20,7 +20,11 @@ dotenv.config();
 app.use(express.json());
 
 app.get("/", (req, res, next) => {
-  res.send("Welcome!");
+  res.json({ msg: "Welcome!" });
+});
+
+app.get("/api/v1", (req, res, next) => {
+  res.json({ msg: "API!" });
 });
 
 app.use("/api/v1/auth", authRouter);
