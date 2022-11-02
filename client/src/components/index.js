50619bf5ae1bbaa3ec0165f92b81bdb7a8b152fc
Your Name <urielparavi75@gmail.com>
2022-11-02 12:02:42 +0200

    play-94-setup-section-21

--- client/src/components/index.js
+++ client/src/components/index.js
@@ -1,5 +1,8 @@
 import FormRow from "./FormRow";
 import Logo from "./Logo";
 import Alert from "./Alert";
+import Navbar from "./Navbar";
+import BigSidebar from "./BigSidebar";
+import SmallSidebar from "./SmallSidebar";
 
-export { Logo, FormRow, Alert };
+export { Logo, FormRow, Alert, Navbar, BigSidebar, SmallSidebar };
